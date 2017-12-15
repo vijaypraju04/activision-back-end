0fcd143536cba0a944b01b2e1ec2261d4edf0a96
Vijay Raju <vijaypraju@gmail.com>
2017-12-14 21:07:09 -0500

    apis up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+  namespace :v1 do
+    resources :users, only: [:index, :create, :show]
+    resources :events, only: [:index, :show, :create]
+    resources :categories, only: [:index, :show, :create]
+    post '/login', to: 'sessions#create'
+    get '/current_user', to: 'sessions#show'
+  end
+end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "vijaypraju04", password: "kenneth", first_name: "Vijay", last_name: "Raju", address: "590 Canterbury Drive", picture: "http://www.penguins-world.com/wp-content/uploads/emperor_chicks.jpg", email:"vijaypraju04@gmail.com", bio: "I am the CEO of this application", birth_date: "October 8, 1993")
+
+Event.create(title: "Weekly Impeach Protest: Sexual Assault", location: "Fox News Building", description: "Join Rise and Resist as we call for IMPEACHMENT of the 45th President for multiple acts of SEXUAL ASSAULT.
+
+We will gather outside the NYC headquarters of Fox News, 1211 Avenue of the Americas at 5:30 pm, Thursday December 14 to voice our collective rage that a sexual abuser leads our nation. From the misogynist network that props up the Predator-in-Chief, we will march to Trump Tower and together call out IMPEACH and WE BELIEVE THE WOMEN and TRUMP, YOU'RE FIRED.
+
+Men who harass and hurt women are finally losing their jobs. Now it's his turn.
+
+#WeBelievetheWomen #HisTurn", picture: "nothing", address: "1211 Avenue of the Americas, New York", date: "December 14, 2017", time: "5:30 PM")
+
+Category.create(name: "Protest")
+
+UserEvent.create(user_id: 1, event_id: 1)
+
+EventCategory.create(event_id: 1, category_id: 1)
